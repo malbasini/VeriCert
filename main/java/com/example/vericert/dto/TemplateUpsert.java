8e1c73fbfe5f460ecf5920fbcec16ffb1eeb9481
malbasini <malbasini@gmail.com>
2025-10-09 20:03:37 +0200

    create.html

--- main/java/com/example/vericert/dto/TemplateUpsert.java
+++ main/java/com/example/vericert/dto/TemplateUpsert.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 public record TemplateUpsert(
         @NotBlank(message = "Nome del template obbligatorio") String name,
         @NotBlank(message="Versione obbligatoria") String version,
-        @NotBlank(message = "Il compo html è obbligatorio") String html,
+        @NotBlank(message = "Il campo html è obbligatorio") String html,
         String variablesJson, // opz
         boolean active
 ) {}
