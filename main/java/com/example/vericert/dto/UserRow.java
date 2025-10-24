0072be409b91344fac2b68cc43cd4e3b68fdb269
malbasini <malbasini@gmail.com>
2025-10-24 07:46:34 +0200

    Users Details

--- main/java/com/example/vericert/dto/UserRow.java
+++ main/java/com/example/vericert/dto/UserRow.java
@@ -3,7 +3,7 @@ package com.example.vericert.dto;
 import com.example.vericert.enumerazioni.Status;
 
 public record UserRow(
-        Long userId,          // tipo uguale a u.id (Long, UUID, ecc.)
+        Long id,          // tipo uguale a u.id (Long, UUID, ecc.)
         String userName,
         String email,
         com.example.vericert.enumerazioni.Role role,
